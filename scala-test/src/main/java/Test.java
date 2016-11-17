93ef8d43b7ea3c4371c574fd29f9165509828ffd
jinzhou <jinzhou@tethrnet.com>
2016-11-17 12:11:29 +0800

    begin spark

---
+++ scala-test/src/main/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jinzhou on 11/17/16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ scala-test/src/main/scala/test.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by jinzhou on 11/17/16.
+  */
+object test {
+  def main(args: Array[String]): Unit = {
+    print("test")
+  }
+
+}
